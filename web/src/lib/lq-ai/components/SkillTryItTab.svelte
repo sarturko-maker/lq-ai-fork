73c50e887b6dbb2a47130965bc1c27fb99f8b2d2
Kevin-Tucuxi <kevin@tucuxi.ai>
2026-05-13 21:51:11 -0700

    feat(web): SkillTryItTab wrapper (Wave D.2)
    Signed-off-by: Kevin-Tucuxi <kevin@tucuxi.ai>

---
+++ web/src/lib/lq-ai/components/SkillTryItTab.svelte
@@ -0,0 +1,7 @@
+<!-- web/src/lib/lq-ai/components/SkillTryItTab.svelte -->
+<script lang="ts">
+  import SkillTryItPane from './SkillTryItPane.svelte';
+  export let skillSlug: string;
+</script>
+
+<SkillTryItPane {skillSlug} source="tryit-tab" />
